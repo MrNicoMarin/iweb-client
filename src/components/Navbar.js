4420e3d2791c0ee0f90ac8c414d5b6187f597999
MrNicoMarin <nicolasm008@gmail.com>
2021-12-03 17:33:17 +0100

    Primer commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import logo from '../logo.svg';
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function NavbarWeb () {
+    return (<Navbar fixed="top" bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img 
+                            src={logo}
+                            alt="Logo"
+                            className="d-inline-block align-top"
+                            width="30"
+                            height="30"
+                        />{' '}
+                    Zoomcar
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav className="me-auto my-2 my-lg-0" navbarScroll style={{ maxHeight: '150px' }} >
+                            <Nav.Link href="/trayectos">Trayectos</Nav.Link>
+                            <Nav.Link href="/vehiculos">Vehiculos</Nav.Link>
+                            <Nav.Link href="/usuarios">Usuarios</Nav.Link>
+                        </Nav>
+                    </ Navbar.Collapse>
+                </Container>
+            </Navbar>);
+}
+
+export default NavbarWeb;
